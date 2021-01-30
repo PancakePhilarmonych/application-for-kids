cfb31b388cb9ef3472e3fa0120ff2a323024cacc
Alexandr Panfilov <rikko.alexandr@gmail.com>
2021-01-31 02:28:43 +0300

    add web-only alert

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="navigation">
+  <div class="web">
+    <div class="navigation">
     <router-link class="option" to="/">Drawing</router-link>
     <router-link class="option" to="/words-board">Words</router-link>
+    </div>
+    <router-view></router-view>
   </div>
-  <router-view></router-view>
+  <div class="web-only">Try it on your laptop or pc :)</div>
 </template>
